273272dbd8f55aaf39ddcb5c9759dc022719a284
Caleb Leaman <78056352+calebleaman@users.noreply.github.com>
2021-02-01 19:11:30 -0400

    Add files via upload

---
+++ mario_level_1.py
@@ -0,0 +1,14 @@
+########################
+# Author: Caleb Leaman #
+########################
+
+import sys
+import pygame as pg
+from data.main import main
+import cProfile
+
+
+if __name__=='__main__':
+    main()
+    pg.quit()
+    sys.exit()
